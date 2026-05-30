1506bce41b1010bf327b823791d1c9a66e9168a2
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-05-31 02:05:53 +0600

    Refactor useSecureAxios to standardize token retrieval and clean up interceptor comments

--- src/hooks/useSecureAxios.jsx
+++ src/hooks/useSecureAxios.jsx
@@ -5,37 +5,35 @@ import useAuth from "./useAuth";
 const axiosSecure = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
 const useAxiosSecure = () => {
   const navigate = useNavigate();
   const { logOut } = useAuth();
 
-  // Add a request interceptor
   axiosSecure.interceptors.request.use(
     function (config) {
-      const token = localStorage.getItem("access-token");
+      const token = localStorage.getItem("token");
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
     function (error) {
-      // Do something with request error
       return Promise?.reject(error);
-    }
+    },
   );
 
-   // Add a response interceptor
-   axiosSecure.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-}, async (error) => {
-    const status = error?.response?.status;
-    if (status === 401 || status === 403) {
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error?.response?.status;
+      if (status === 401 || status === 403) {
         await logOut();
-        navigate('/signin');
-    }
-    return Promise.reject(error);
-});
+        navigate("/signIn");
+      }
+      return Promise.reject(error);
+    },
+  );
   return axiosSecure;
 };
-
 export default useAxiosSecure;
