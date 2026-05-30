e15e36c4ee061a2ec4ab812666b4f2d2aa334ef6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-05-31 02:04:49 +0600

    Fix token retrieval in PrivateRoute component

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,14 +1,13 @@
+import PropTypes from "prop-types";
 import useAuth from "@/hooks/useAuth";
 import Loading from "@/Pages/Common/Loading";
-import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
-  const token = localStorage.getItem("access-token");
+  const token = localStorage.getItem("token");
   const location = useLocation();
   if (loading) return <Loading />;
-  console.log("Loading State:",loading);
 
   if (user && token) {
     return children;
