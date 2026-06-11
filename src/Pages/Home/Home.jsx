7c7db477ded9671b7561b1b54d2ac2142748565f
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-06-12 05:01:57 +0600

    Add TransformYourself and ProPlan to Home
    Import and render two new homepage sections: TransformYourself and ProPlan. Added their import statements and inserted <TransformYourself/> and <ProPlan/> into the ContentWrapper after SuccessStories to integrate the new sections into the Home page layout.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import AboutSection from "@/components/pages/Home/AboutSection";
 import HeroSection from "@/components/pages/Home/HeroSection";
+import ProPlan from "@/components/pages/Home/ProPlan";
 import SuccessStories from "@/components/pages/Home/SuccessStories";
+import TransformYourself from "@/components/pages/Home/TransformYourself";
 import ContentWrapper from "@/context/ContentWrapper";
 
 const Home = () => {
@@ -10,6 +12,8 @@ const Home = () => {
             <ContentWrapper>
                 <AboutSection />
                 <SuccessStories/>
+                <TransformYourself/>
+                <ProPlan/>
             </ContentWrapper>
         </div>
     )
