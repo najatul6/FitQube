6511bccd85ac3bb7ef1311d27892250d2bdc9e2e
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-06-11 22:53:21 +0600

    Update CallButton component with animation and styling adjustments

--- src/components/shared/CallButton.jsx
+++ src/components/shared/CallButton.jsx
@@ -3,9 +3,9 @@ import { FaPhoneAlt } from "react-icons/fa";
 const CallButton = () => {
     return (
         <a href="tel:+8801581205392" className="fixed bottom-6 right-6 z-999">
-            <div className="relative flex items-center justify-center">
+            <div className="relative flex items-center justify-center animate-bounce">
                 {/* animated ring */}
-                <span className="absolute inline-flex h-full w-full rounded-2xl bg-primary-200 opacity-60 animate-ping"></span>
+                <span className="absolute inline-flex h-full w-full rounded-2xl bg-black"></span>
 
                 {/* button */}
                 <div className="relative w-16 h-16 rounded-2xl bg-primary-200 text-white flex items-center justify-center shadow-2xl hover:scale-110 transition duration-300">
