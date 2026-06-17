8adffaa57d3ec43c3246e5824e69422d4dfda626
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2026-06-18 02:29:51 +0600

    Import additional About page sections
    Add imports for MeetTrainer, StatsSection, Testimonials, TransformationProcess, and WhyChooseUs in About.jsx to prepare the About page for rendering these new sections.

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,19 +1,27 @@
 import AboutHero from "@/components/pages/About/AboutHero"
 import AboutStory from "@/components/pages/About/AboutStory"
+import MeetTrainer from "@/components/pages/About/MeetTrainer"
+import StatsSection from "@/components/pages/About/StatsSection"
+import Testimonials from "@/components/pages/About/Testimonials"
+import TransformationProcess from "@/components/pages/About/TransformationProcess"
+import WhyChooseUs from "@/components/pages/About/WhyChooseUs"
 import CTA from "@/components/pages/Home/CTA"
+import ContentWrapper from "@/context/ContentWrapper"
 
 const About = () => {
   return (
-    <>
-      <AboutHero />
-      <AboutStory />
-      <WhyChooseUs />
-      <StatsSection />
-      <MeetTrainer />
-      <TransformationProcess />
-      <Testimonials />
-      <CTA />
-    </>
+    <ContentWrapper>
+      <div className="space-y-12 pt-5 px-6 lg:px-0">
+        <AboutHero />
+        <AboutStory />
+        <WhyChooseUs />
+        <StatsSection />
+        <MeetTrainer />
+        <TransformationProcess />
+        <Testimonials />
+        <CTA />
+      </div>
+    </ContentWrapper>
   )
 }
 
